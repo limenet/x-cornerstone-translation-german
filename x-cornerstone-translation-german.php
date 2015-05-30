1f01d04cf94930725d222c17bde7d9a83fbf9011
Linus Metzler <linus.metzler@gmail.com>
2015-05-30 17:39:17 +0200

    added plugin header

---
+++ x-cornerstone-translation-german.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: German Translation for the Cornerstone plugin
+ * Version: 0.1
+ * Plugin URI: https://github.com/limenet/x-cornerstone-translation-german
+ * Description: A non-official translation for the Cornerstone plugin into German
+ * Author: Linus Metzler
+ * Author URI: http://linusmetzler.me
+ * License: GNU General Public License v2 or later
+ * GitHub Plugin URI: limenet/x-cornerstone-translation-german
+ */
+
+
+load_theme_textdomain( '__x__', __DIR__ . '/' );
+
